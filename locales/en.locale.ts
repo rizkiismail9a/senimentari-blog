f96332ac1982b8c7248385a357b84b97b9260808
Muhamad Rizki Ismail <laksmimentari@gmail.com>
2024-10-19 06:31:03 +0700

    Update en.locale.ts

--- locales/en.locale.ts
+++ locales/en.locale.ts
@@ -5,7 +5,7 @@ import { aboutPageEn } from "./pages/about.locale";
 export default {
   home: "Home",
   about: "About",
-  "home-header": "Article Collection",
+  "home-header": "senimentari blog",
   "home-header-desc":
     "This website offers tips, tricks, and creative solutions to help fix bugs and configuration issues in software development. Designed especially for beginner developers looking to learn easier and more efficiently.",
   "search-placeholder": "find title and tag of articles",
